bb762617ec48a52aec7d31f791c469755f1f326b
Sasha Koshka <supergriffin64@gmail.com>
2021-05-11 00:07:25 -0400

    Command prompt doesn't pop up anymore when running on windows

--- win-build.bat
+++ win-build.bat
@@ -1,2 +1,1 @@
-gcc main.c -Iwin\SDL2\include -Lwin\SDL2\lib -Wall -lmingw32 -lSDL2main -lSDL2 -o win\m4kc
-pause
+gcc main.c -mwindows -Iwin\SDL2\include -Lwin\SDL2\lib -Wall -lmingw32 -lSDL2main -lSDL2 -o win\m4kc
