d8ec6296008fee5fe2252a6d1b90f9b29243b06f
Sasha Koshka <supergriffin64@gmail.com>
2022-04-27 00:44:43 -0400

    Moved more mystery variables back into gameLoop

--- src/gameloop.c
+++ src/gameloop.c
@@ -17,34 +17,6 @@
  * psychological effects.                                                    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-float
-        f9,
-        f10,
-        f11,
-        f12,
-        f16,
-        f17,
-        f18,
-        f19,
-        f20,
-        f21,
-        f22,
-        f23,
-        f24,
-        f25,
-        f26,
-        f27,
-        f28,
-        f29,
-        f30,
-        f31,
-        f32,
-        f33,
-        f34,
-        f35,
-        f36,
-        timeCoef;
-
 long l, gameTime;
 
 int
@@ -111,6 +83,34 @@ int gameLoop (
         World *world,
         SDL_Renderer *renderer
 ) {
+  static float
+        f9,
+        f10,
+        f11,
+        f12,
+        f16,
+        f17,
+        f18,
+        f19,
+        f20,
+        f21,
+        f22,
+        f23,
+        f24,
+        f25,
+        f26,
+        f27,
+        f28,
+        f29,
+        f30,
+        f31,
+        f32,
+        f33,
+        f34,
+        f35,
+        f36,
+        timeCoef;
+
   static double d;
 
   static int
