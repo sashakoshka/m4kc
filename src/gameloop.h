ca0e0cc6611e48e1355fccaa9a761ecaf65c6a81
Tempetas <tampetus@gmail.com>
2023-05-17 23:12:39 +0200

    Almost completely fix the rendering bugs in the negatives

--- src/gameloop.h
+++ src/gameloop.h
@@ -4,8 +4,6 @@
 #include "terrain.h"
 #include "main.h"
 #include "player.h"
-
-#define PLAYER_POSITION_OFFSET 128
  
 int  gameLoop            (Inputs *inputs, SDL_Renderer *renderer);
 void gameLoop_resetGame  ();
